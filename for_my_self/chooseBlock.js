424e9b65f77b00c98864570ab33910125a70b68d
jarryzeng <jaayzeng@gmail.com>
2023-09-24 16:16:01 +0800

    2023-09-24.16:15

--- for_my_self/chooseBlock.js
+++
@@ -1,3 +0,0 @@
-let block = ["船場","電力網","裂縫","花園","診所","搏鬥之夜","貨船","綠洲","極樂世界","水耕","相移驅動器","盆景廣場","伊卡洛斯","太陽能矩陣","軌道加農砲","栽培塔"];
-let print = block[Math.floor(Math.random()*block.length)];
-console.log(print);
--- for_my_self/rewrite_file.js
+++ for_my_self/rewrite_file.js
@@ -1,24 +1,55 @@
 const fs = require("fs");
-const path = "D:\\Visual studio code\\project\\petStore\\member\\member_data\\";
+let path;
 
-fs.readdir(`${path}`, (err, files) => {
-    if(err) console.log(err);
-    else{
-        files.map((fname)=>{
-            fs.readFile(`${path}${fname}`, (err, detail) => {
-                if(err) console.log(err);
-                else{
-                    let file = JSON.parse(detail);
-                    if(file["contact-data-number-2"] != undefined){
-                        let value = file["contact-data-number-2"];
-                        delete file["contact-data-number-2"];
-                        file["contact-data-phone_number-2"] = value;
-                        fs.writeFile(`${path}${fname}`, JSON.stringify(file), (err) => {
-                            if(err) console.log(err);
-                        });
-                    }
-                }
-            });
-        });
+async function writeFile(location, content){
+    try{
+        await fs.writeFile(location, content);
     }
-});
+    catch(err){
+        console.log("Error from write file:");
+        console.log(err);
+    }
+}
+
+async function readFile(location, fileName){
+    try{
+        await fs.readFile(`${location}${fileName}`);
+    }
+    catch(err){
+        console.log("Error from read file:");
+        console.log(err);
+    }
+}
+
+async function readdir(location){
+    try{
+        let files = await fs.readdir(location);
+        files.map((fileName) => readFile(location, fileName));
+    }
+    catch(err){
+        console.log("Error from read directory:");
+        console.log(err);
+    }
+}
+
+// fs.readdir(`${path}`, (err, files) => {
+//     if(err) console.log(err);
+//     else{
+//         files.map((fname)=>{
+//             fs.readFile(`${path}${fname}`, (err, detail) => {
+//                 if(err) console.log(err);
+//                 else{
+//                     let file = JSON.parse(detail);
+//                     if(file["contact-data-number-2"] != undefined){
+//                         let value = file["contact-data-number-2"];
+//                         delete file["contact-data-number-2"];
+//                         file["contact-data-phone_number-2"] = value;
+//                         fs.writeFile(`${path}${fname}`, JSON.stringify(file), (err) => {
+//                             if(err) console.log(err);
+//                         });
+//                     }
+//                 }
+//             });
+//         });
+//     }
+// });
