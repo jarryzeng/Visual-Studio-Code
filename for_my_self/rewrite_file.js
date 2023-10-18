8eeea7d39f37887294c0a97439a201b4667c00bb
jarryzeng <66196813+jarryzeng@users.noreply.github.com>
2023-10-18 23:19:24 +0000

    2023-10-19.07:19

--- for_my_self/rewrite_file.js
+++ for_my_self/rewrite_file.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const buffer = require("buffer");
+const { isNullOrUndefined } = require("util");
 
 function printError(local, err){
     console.log(`\nError from ${local}`);
@@ -11,54 +12,44 @@ async function writeFile(location, content){
     try{
         await fs.promises.writeFile(location, content);
     }
-    catch(err){ printError('write file', err); }
+    catch(err){ 
+        printError('write file', err);
+    }
 }
 
-async function readFile(location, fileName, search){
+async function readFile(location, fileName, search, callback){
     try{
         let file = await fs.promises.readFile(`${location}${fileName}`, 'utf8');
         if(fileName != search && search != '*'){
             printError('read file', 'the file name is different');
             return;
         };
-        writeFile(`${location}${fileName}`, file);
+        return file;
     }
     catch(err){
         switch(err.errno){
             case -4068: printError('read file', 'this is directory'); break;
+            case -21: printError('read file', 'you are trying to do something on the directory'); break;
             default: printError('read file', err);
         }
     }
 }
 
-async function readdir(location, search){
+async function readdir(location="./", search, callback=undefined){
     try{
         let files = await fs.promises.readdir(location);
-        files.map((fileName) => readFile(location, fileName, search));
+        files.map((fileName) => readFile(location, fileName, search, callback));
     }
     catch(err){ printError('read directory', err); }
 }
 
-readdir("./", "rewrite_file.js");
+let filename = "path.txt";
+let fileContent = readdir("./", filename);
 
-// fs.readdir(`${path}`, (err, files) => {
-//     if(err) console.log(err);
-//     else{
-//         files.map((fname)=>{
-//             fs.readFile(`${path}${fname}`, (err, detail) => {
-//                 if(err) console.log(err);
-//                 else{
-//                     let file = JSON.parse(detail);
-//                     if(file["contact-data-number-2"] != undefined){
-//                         let value = file["contact-data-number-2"];
-//                         delete file["contact-data-number-2"];
-//                         file["contact-data-phone_number-2"] = value;
-//                         fs.writeFile(`${path}${fname}`, JSON.stringify(file), (err) => {
-//                             if(err) console.log(err);
-//                         });
-//                     }
-//                 }
-//             });
-//         });
-//     }
-// });
+/*
+for(let i = 0;i < fileContent.length;i++){
+    if(fileContent[i] === ':') fileContent[i] = '/n';
+}
+
+writeFile("./path.txt", fileContent);
+*/
