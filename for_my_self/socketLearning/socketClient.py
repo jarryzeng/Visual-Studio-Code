d9d9928d12f303ef190f1c6c1643d0b65bcdf174
jarryzeng <jaayzeng@gmail.com>
2023-11-26 10:11:27 +0800

    2023-11-26.10:11

--- for_my_self/socketLearning/socketClient.py
+++ for_my_self/socketLearning/socketClient.py
@@ -1,17 +1,31 @@
 import socket
-host = '120.124.135.96'
+host = '10.147.20.138'
 port = 7
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(10)
 s.connect((host, port))
 
-while True:
-    outdata = input('please input message:')
-    s.send(outdata.encode())
+print('method:')
+print('1.start - open the computer')
+print('2.stop - disconnect to the bridge')
+print('3.bridge stop - turn off the bridge')
+print('4.server connect - connect to server')
+print('5.server disconnect - disconnect to server')
+print('6.server stop - turn off the server')
+print('every request most waiting for 10 second')
 
-    indata = s.recv(1024)
-    if len(indata) == 0:
-        s.close()
-        print('server closed connection')
-        break
-    print(f'recv: {indata.decode()}', end='\n\n')
+while True:
+    try:
+        outdata = input('\ninput command:')
+        s.send(outdata.encode())
+
+        indata = s.recv(1024)
+        if len(indata) == 0:
+            s.close()
+            print('server closed connection')
+            break
+        print(f'recv: {indata.decode()}')
+    except TimeoutError as timeout:
+        print('time out')
+        print('please input again')
