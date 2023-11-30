ca495961d2c09755be62f183f1dcbc1ccafae367
jarryzeng <jaayzeng@gmail.com>
2023-12-01 01:22:54 +0800

    2023-12-1.01:22

---
+++ myBase/findSimilarities/filterColor.py
@@ -0,0 +1,4 @@
+import cv2
+
+img = cv2.imread("img.jpg")
+screen = cv2.imread("screen.jpg")
--- myBase/findSimilarities/getPicture.py
+++ myBase/findSimilarities/getPicture.py
@@ -1,14 +1,15 @@
 import os
-from datetime import datetime
+import numpy as np
 from pynput import keyboard
-from PIL.ImageGrab import grab
-from pyautogui import position
+import pyautogui
+import cv2
 
 class main:
     def __init__(self, startPosition, endPosition):
         self.startPosition = startPosition
         self.endPosition = endPosition
         self.isPress = True
+        self.local = "./findSimilarities/"
     
     def listener(self):
         self.listen = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
@@ -18,16 +19,20 @@ class main:
     def on_press(self, key):
         if key == keyboard.KeyCode.from_char("\'") and self.isPress:
             self.isPress = False
-            x, y = position()
+            x, y = pyautogui.position()
             self.startPosition = [x, y]
         elif key == keyboard.Key.esc:
-            os.system(f"rm {self.name}")
+            try:
+                os.remove(f"{self.local}img.jpg")
+                os.remove(f"{self.local}screen.jpg")
+            except Exception as exc:
+                print(exc)
             self.listen.stop()
 
     def on_release(self, key):
         if key == keyboard.KeyCode.from_char("\'"):
             self.isPress = True
-            x, y = position()
+            x, y = pyautogui.position()
             self.endPosition = [x, y]
             self.makePicture(self.startPosition, self.endPosition)
 
@@ -36,14 +41,24 @@ class main:
         y = [start[1], end[1]]
         if(x[1] < x[0]): x[1], x[0] = x[0], x[1]
         if(y[1] < y[0]): y[1], y[0] = y[0], y[1]
-        bbox = (x[0], y[0], x[1], y[1])
-        print(os.times())
-        time = datetime.now()
-        self.name = str(time.strftime("%Y-%m-%d")) + '.jpg'
+        width = x[1] - x[0]
+        height = y[1] - y[0]
+        position = (x[0], y[0], width, height)
+        self.name = "img.jpg"
 
         try:
-            img = grab(bbox = bbox)
-            img.save("./findSimilarities/" + self.name)
+            img = pyautogui.screenshot("img.jpg", region=position)
+            img = np.array(img)
+            screen = pyautogui.screenshot()
+            screen = np.array(screen)
+
+            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+            cv2.cvtColor(screen, cv2.COLOR_BGRA2GRAY)
+            cv2.imshow("img", img)
+            # cv2.imshow("screen", screen)
+            cv2.waitKey(1)
+            cv2.imwrite(f"{self.local}screen.jpg", screen)
+            cv2.imwrite(f"{self.local}img.jpg", img)
         except Exception as e:
             print(e)
         
